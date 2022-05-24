071e649ed0ecfbe39cec89bc300eb20cfe4aa1e8
Halit Mesut <halitmesut@gmail.com>
2022-05-24 19:37:03 +0300

    first git commiting

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.yellow,
+          title: const Center(
+            child: Text("I'am Poor"),
+          ),
+          foregroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.grey,
+        body: const Center(
+          child: Image(
+            image: AssetImage("images/prisoner.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
